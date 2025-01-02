1b36f8aa46826ea96acbbef586fbca151e6f8fbd
Mr.K <48143260+mrkumao@users.noreply.github.com>
2025-01-02 20:28:07 +0900

    KEYBALL_SCROLLSNAP_ENABLE 0を無効化し、垂直、水平、自由を切り替え可能にした。 あと、PERMISSIVE_HOLDとRETRO_TAPPINGも無効化

--- keyboards/keyball/keyball39/keymaps/mrk/config.h
+++ keyboards/keyball/keyball39/keymaps/mrk/config.h
@@ -39,6 +39,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
 #define AUTO_MOUSE_LAYER_KEEP_TIME 30000
-#define KEYBALL_SCROLLSNAP_ENABLE 0
-#define PERMISSIVE_HOLD
-#define RETRO_TAPPING
+//#define KEYBALL_SCROLLSNAP_ENABLE 0
+//#define PERMISSIVE_HOLD
+//#define RETRO_TAPPING
