926746865ef81507abcece3657108d5a007d7254
Mr.K <48143260+mrkumao@users.noreply.github.com>
2025-01-02 20:28:07 +0900

    config.h にPERMMISIVE_HOLD とRETRO_TAPPINGを追記

--- keyboards/keyball/keyball39/config.h
+++ keyboards/keyball/keyball39/config.h
@@ -29,7 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SOFT_SERIAL_PIN         D2
 #define SPLIT_HAND_MATRIX_GRID  F6, B5
 #define SPLIT_USB_DETECT
-#define SPLIT_USB_TIMEOUT       500
+#define SPLIT_USB_TIMEOUT       2500
 #ifdef OLED_ENABLE
 #    define SPLIT_OLED_ENABLE
 #endif
--- keyboards/keyball/keyball39/keymaps/mrk/config.h
+++ keyboards/keyball/keyball39/keymaps/mrk/config.h
@@ -34,8 +34,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 #define TAP_CODE_DELAY 5
+#define TAPPING_TERM 180
 //#define DYNAMIC_KEYMAP_LAYER_COUNT 8
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
 #define AUTO_MOUSE_LAYER_KEEP_TIME 30000
 #define KEYBALL_SCROLLSNAP_ENABLE 0
+#define PERMISSIVE_HOLD
+#define RETRO_TAPPING
